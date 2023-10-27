0f3dca9015459b50998c87a1f83eb98bd7092ebb
Ryan McCormick <rmccormick@nvidia.com>
2023-10-27 15:44:29 -0700

    Formatting

--- src/triton/cli.py
+++ src/triton/cli.py
@@ -2,13 +2,17 @@ import argparse
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(prog='triton', description='CLI to interact with Triton Inference Server')
+    parser = argparse.ArgumentParser(
+        prog="triton", description="CLI to interact with Triton Inference Server"
+    )
     args = parser.parse_args()
     return args
 
 
 def main():
     args = parse_args()
+    print(args)
+
 
 if __name__ == "__main__":
     main()
