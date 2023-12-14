37f84de32abe8e5698ad794abc3fc7268c169945
Katherine Yang <katheriney@nvidia.com>
2023-12-14 12:27:51 -0800

    cleanups

--- src/triton_cli/json_parser.py
+++ src/triton_cli/json_parser.py
@@ -9,17 +9,13 @@ def parse_and_substitute(triton_model_dir, engine_dir, token_dir, token_type, dr
     with open(json_path) as j:
         config_file = json.load(j)
 
-    print(config_file)
-    print(type(config_file))
     json.dumps(config_file)
     config_dict = {}
     config_dict["engine_dir"] = engine_dir
     config_dict["tokenizer_dir"]=token_dir
     config_dict["tokenizer_type"]=token_type
     config_dict["triton_max_batch_size"]=config_file["builder_config"]["max_batch_size"]
-
-    print(config_dict)
-
+    
     trtllm_filepath = triton_model_dir + 'tensorrt_llm/config.pbtxt'
     substitute(trtllm_filepath, config_dict, dry_run)
     preprocessing_filepath = triton_model_dir + 'preprocessing/config.pbtxt'
@@ -31,9 +27,7 @@ def parse_and_substitute(triton_model_dir, engine_dir, token_dir, token_type, dr
 def substitute(file_path, sub_dict, dry_run):
     with open(file_path) as f:
         pbtxt = Template(f.read())
-
     pbtxt = pbtxt.safe_substitute(sub_dict)
-
     if not dry_run:
         with open(file_path, "w") as f:
             f.write(pbtxt)
@@ -46,14 +40,15 @@ if __name__ == "__main__":
     parser.add_argument("--triton_model_dir", "-t", help="path of the .pbtxt to modify")
     parser.add_argument("--engine_dir", "-e", help="directory of the engine")
     parser.add_argument("--token_dir", "-m", help="directory of the tokens, usually in the downloaded model folder")
-    parser.add_argument("--token_type", help="default is llama")
+    parser.add_argument("--token_type", help="type of tokens specified in token_dir. Default is llama")
     parser.add_argument("--dry_run",
                         "-d",
                         action="store_true",
                         help="do the operation in-place")
     FLAGS = parser.parse_args()
-    if FLAGS.token_dir is None :
+    if FLAGS.token_dir is None:
         FLAGS.token_dir = FLAGS.engine_dir
-    FLAGS.token_type = "llama"
+    if FLAGS.token_type is None:
+        FLAGS.token_type = "llama"
 
     parse_and_substitute(FLAGS.triton_model_dir, FLAGS.engine_dir, FLAGS.token_dir, FLAGS.token_type, FLAGS.dry_run)
