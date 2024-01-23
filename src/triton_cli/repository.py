c8f358a2a08dfe6413ffc846bd219986aef77c57
Francesco Petrini <francescogpetrini@gmail.com>
2024-01-22 16:21:01 -0800

    Minor Repo Optimizations (#30)
    * Minor quality-of-life repo optimizations
    ---------
    
    Co-authored-by: Ryan McCormick <rmccormick@nvidia.com>

--- src/triton_cli/repository.py
+++ src/triton_cli/repository.py
@@ -73,8 +73,14 @@ ENGINE_DEST_PATH = os.environ.get("ENGINE_DEST_PATH", "/tmp/engines")
 HF_TOKEN_PATH = Path.home() / ".cache" / "huggingface" / "token"
 
 SUPPORTED_TRT_LLM_BUILDERS = {
-    "gpt2": GPTBuilder,
-    "meta-llama/Llama-2-7b-hf": LlamaBuilder,
+    "gpt2": {
+        "builder": GPTBuilder,
+        "hf_allow_patterns": ["*.safetensors", "*.json"],
+    },
+    "meta-llama/Llama-2-7b-hf": {
+        "builder": LlamaBuilder,
+        "hf_allow_patterns": ["*.safetensors", "*.json"],
+    },
 }
 
 
@@ -242,22 +248,38 @@ class ModelRepository:
             raise ValueError("HuggingFace ID must be non-empty")
 
         if backend == "tensorrtllm":
-            Builder = SUPPORTED_TRT_LLM_BUILDERS.get(huggingface_id)
-            if not Builder:
+            builder_info = SUPPORTED_TRT_LLM_BUILDERS.get(huggingface_id)
+            if not builder_info:
                 raise NotImplementedError(
                     f"Building a TRT LLM engine for {huggingface_id} is not currently supported."
                 )
+
+            Builder = builder_info["builder"]
+            hf_allow_patterns = builder_info["hf_allow_patterns"]
+
             engines_path = ENGINE_DEST_PATH + "/" + name
             tokenizer_path = ENGINE_DEST_PATH + "/" + name + "/tokenizer"
 
-            self.__download_hf_model(huggingface_id, tokenizer_path)
+            engines = [engine for engine in Path(engines_path).glob("*.engine")]
+            if engines:
+                logger.warning(
+                    f"Found existing engine(s) at {engines_path}, skipping build."
+                )
+            else:
+                self.__download_hf_model(
+                    huggingface_id, tokenizer_path, allow_patterns=hf_allow_patterns
+                )
 
-            builder = Builder(
-                tokenizer_path=tokenizer_path,
-                engine_output_path=engines_path,
-            )
-            builder.build()
+                builder = Builder(
+                    tokenizer_path=tokenizer_path,
+                    engine_output_path=engines_path,
+                )
+                builder.build()
 
+            # NOTE: In every case, the TRT LLM template should be filled in with values.
+            # If the model exists, the CLI will raise an exception when creating the model repo.
+            # If a user clears the model repo, they won't need to re-build the engines,
+            # but they will still need to modify the TRT LLM template.
             parse_and_substitute(
                 str(self.repo),
                 name,
@@ -277,15 +299,15 @@ class ModelRepository:
                 model_file = version_dir / file
                 model_file.write_text(contents)
 
-    def __download_hf_model(self, huggingface_id: str, tokenizer_path: str):
+    def __download_hf_model(
+        self, huggingface_id: str, tokenizer_path: str, allow_patterns: list = []
+    ):
         # Shouldn't require the user to authenticate with HF unless
         # necessary (i.e., the model exists in a gated repo)
-
-        # TODO: Determine minimal set of files needed across all engine builders
         try:
             snapshot_download(
                 huggingface_id,
-                ignore_patterns=["onnx*"],
+                allow_patterns=allow_patterns,
                 local_dir=tokenizer_path,
             )
         except hf_utils.GatedRepoError:
@@ -295,7 +317,7 @@ class ModelRepository:
                 )
             snapshot_download(
                 huggingface_id,
-                ignore_patterns=["onnx*"],
+                allow_patterns=allow_patterns,
                 local_dir=tokenizer_path,
                 use_auth_token=True,  # for gated repos like llama
             )
