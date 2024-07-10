a9944b0098f4dac3ceec86f6f2ca0bb92198f6c2
Ryan McCormick <rmccormick@nvidia.com>
2024-07-10 14:17:53 -0700

    Shorten genai-perf run criteria to speedup local testing and github action pipelines

--- tests/utils.py
+++ tests/utils.py
@@ -74,6 +74,9 @@ class TritonCommands:
 
     def _profile(model, backend):
         args = ["profile", "-m", model, "--backend", backend]
+        # NOTE: With default parameters, genai-perf may take upwards of 1m30s or 2m to run,
+        # so limit the genai-perf run with --request-count to reduce time for testing purposes.
+        args += ["--", "--request-count", "10"]
         run(args)
 
     def _metrics():
